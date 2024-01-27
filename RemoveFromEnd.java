3e53054ba5f6595124825f8e7809f7c0af20b71f
CheChc <3331394002@qq.com>
2024-01-28 00:38:11 +0800

    删除链表倒数第n个元素

---
+++ RemoveFromEnd.java
@@ -0,0 +1,20 @@
+public class RemoveFromEnd
+{
+    public ListNode removeNthFromEnd(ListNode head, int n)
+    {
+        ListNode dummyHead= new ListNode(0);
+        dummyHead.next=head;
+        ListNode fast=dummyHead,slow=dummyHead;
+        for(int i=0;i<=n&&fast!=null;i++)
+        {
+            fast=fast.next;
+        }
+        while(fast!=null)
+        {
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return dummyHead.next;
+    }
+}
