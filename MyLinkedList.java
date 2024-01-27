cabb32850162d0d13f34bacb784359499c913573
CheChc <3331394002@qq.com>
2024-01-27 16:22:02 +0800

    reserve linkList

---
+++ MyLinkedList.java
@@ -0,0 +1,105 @@
+class LinkedListNode
+{
+    int val;
+    LinkedListNode next;
+
+    LinkedListNode()
+    {
+    }
+
+    LinkedListNode(int val)
+    {
+        this.val = val;
+    }
+}
+
+class MyLinkedList
+{
+    int size;//链表元素的个数
+    LinkedListNode head;//虚拟头节点
+
+    public MyLinkedList()
+    {
+        size = 0;
+        head = new LinkedListNode(0);
+    }
+
+    public int get(int index)
+    {
+        if (index < 0 || index >= size)
+            return -1;
+        LinkedListNode cur = head;
+        for(int i=0;i<=index;i++)
+        {
+            cur = cur.next;
+        }
+        return cur.val;
+    }
+
+    public void addAtHead(int val)
+    {
+        addAtIndex(0, val);
+    }
+
+    public void addAtTail(int val)
+    {
+        addAtIndex(size, val);
+    }
+
+    public void addAtIndex(int index, int val)
+    {
+        if (index > size)
+            return;
+        if (index < 0)
+        {
+            index = 0;
+        }
+        size++;
+        LinkedListNode li = new LinkedListNode(val);
+        LinkedListNode cur = head;
+        for(int i=0;i<index;i++)
+        {
+            cur = cur.next;
+        }
+        li.next = cur.next;
+        cur.next = li;
+
+    }
+
+    public void deleteAtIndex(int index)
+    {
+        if (index < 0 || index >= size)
+            return;
+        size--;
+        if(index==0)
+        {
+            head=head.next;
+        }
+        LinkedListNode cur = head;
+        for (int i = 0; i < index; i++)
+        {
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+    }
+
+    public void print()
+    {
+        LinkedListNode cur = head;
+        for (int i = 0; i < size; i++)
+        {
+            cur = cur.next;
+            System.out.println(cur.val);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        MyLinkedList ml = new MyLinkedList();
+        ml.addAtHead(1);
+        ml.addAtTail(2);
+        ml.addAtHead(3);
+        ml.addAtIndex(1, 4);
+        ml.print();
+    }
+}
---
+++ ReversrList.java
@@ -0,0 +1,18 @@
+public class ReversrList
+{
+    public ListNode reverseList(ListNode head)
+    {
+        ListNode cur = head;
+        ListNode pre = null;
+        ListNode temp;
+        while (cur != null)
+        {
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur=temp;
+        }
+        return pre;
+    }
+
+}
